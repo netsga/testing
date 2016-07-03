280911e28c88f45bfa478db8c22e5232721e55ba
Glenn Lee <dongwook.lee@lge.com>
2016-07-03 12:16:26 +0900

    Update env. for windows/eclipse. Java8 must be installed for running.
    Signed-off-by: Glenn Lee <dongwook.lee@lge.com>
    Change-Id: I0a84e23a90e603754aa37007b40c5a51673fb6b2

--- src/main/resources/sql/leafinfo.sql
+++ src/main/resources/sql/leafinfo.sql
@@ -4,9 +4,9 @@ LIMIT 0, 500
 
 -- Date: 2016-06-30 08:49
 */
-INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('getDeviceData','hems','http_get',NULL,'http_get.getDeviceData(localhems,$params, ...)','Content-Type:application/json;charset=utf-8|user-id:${userId}','http://localhost:8080/devices/#{ldId}/data?tags=${convTag}');
+INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('getDeviceData','hems','http_get',NULL,'http_get.getDeviceData(localhems,$params, ...)','Content-Type:application/json;charset=utf-8|user-id:#{userId}','http://localhost:8080/devices/#{ldId}/data?tags=#{tags}');
 INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('getDeviceData','hems','instance',NULL,NULL,NULL,NULL);
 INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('getDeviceData','lime','datamanager',NULL,'datamanager.getDeviceData(plug.dynamicinfo.status.t)',NULL,NULL);
-INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('setDeviceData','hems','http_post','{\\\"request\\\":{\\\"test\\\":#{value}}}','http_post.setDeviceData(localhems, $params, ...)',NULL,'http://localhost:8080/devices/#{ldId}');
+INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('setDeviceData','hems','http_post','{\\\"request\\\":{\\\"#{tagName}\\\":\"#{value}\"}}','http_post.setDeviceData(localhems, $params, ...)',NULL,'http://localhost:8080/devices/#{ldId}');
 INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('setDeviceData','hems','instance',NULL,NULL,NULL,NULL);
 INSERT INTO `tbl_leaf_information` (`method`,`source_name`,`type`,`body`,`description`,`header`,`url`) VALUES ('setDeviceData','lime','datamanager',NULL,'datamanager.setDeviceData(typecode,datasheet)',NULL,NULL);
